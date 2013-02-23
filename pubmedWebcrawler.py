a4b17c1926fd8630b564c4ec1558c3dd3c378f63
Patrick Ye <patrick.p.ye@gmail.com>
2013-02-23 15:50:45 -0800

    fixes

--- pubmedWebcrawler.py
+++ pubmedWebcrawler.py
@@ -9,10 +9,15 @@ import urllib2
 from xml.dom.minidom import parseString
 #from xml.dom import minidom
 
+# import json library for json output
+import json
+
 ## --------------------------------------
 ## get all PMIDs on initial disease query
 ## --------------------------------------
-url = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=pancreatic+neuroendocrine&retmax=20000'
+disease = 'pancreatic+neuroendocrine'
+numPapers = 20
+url = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=' + disease + '&retmax=' + str(numPapers)
 
 ## source: http://www.travisglines.com/web-coding/python-xml-parser-tutorial
 #download the file:
@@ -31,66 +36,87 @@ dom = parseString(data)
 # total number of papers
 countTag = dom.getElementsByTagName('Count')[0].toxml()
 countData = countTag.replace('<Count>','').replace('</Count>','')
+countData = int(countData)
 #print countData
 
-#retrieve the first xml tag (<tag>data</tag>) that the parser finds with name tagName:
-xmlTag = dom.getElementsByTagName('Id')[0].toxml()
-#strip off the tag (<tag>data</tag>  --->   data):
-xmlData=xmlTag.replace('<Id>','').replace('</Id>','')
-#print out the xml tag and data in this format: <tag>data</tag>
-#print xmlTag
-#just print the data
-#print xmlData
+for i in range(countData):
 
-## ------------------------------
-## get XML file from HTTP request
-## ------------------------------
-urlPaper = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=pubmed&id=' + str(xmlData)
-#urlPaper = 'http://www.ncbi.nlm.nih.gov/pubmed/?term=' + str(xmlData) + '&report=xml&format=xml'
-#print(urlPaper)
+	#retrieve the first xml tag (<tag>data</tag>) that the parser finds with name tagName:
+	xmlTag = dom.getElementsByTagName('Id')[i].toxml()
 
-#download the file:
-filePaper = urllib2.urlopen(urlPaper)
-#convert to string:
-dataPaper = filePaper.read()
-#close file because we dont need it anymore:
-filePaper.close()
+	#strip off the tag (<tag>data</tag>  --->   data):
+	PMID=xmlTag.replace('<Id>','').replace('</Id>','')
+	#print out the xml tag and data in this format: <tag>data</tag>
+	#print xmlTag
+	#just print the data
+	print PMID
 
-# print dataPaper
+	## ------------------------------
+	## get XML file from HTTP request
+	## ------------------------------
+	urlPaper = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=pubmed&id=' + str(PMID)
+	#urlPaper = 'http://www.ncbi.nlm.nih.gov/pubmed/?term=' + str(xmlData) + '&report=xml&format=xml'
+	#print(urlPaper)
 
-dom = parseString(dataPaper)
-itemlist = dom.getElementsByTagName('Item') 
-#print len(itemlist)
-#print itemlist[0].attributes['Name'].value
+	#download the file:
+	filePaper = urllib2.urlopen(urlPaper)
+	#convert to string:
+	dataPaper = filePaper.read()
+	#close file because we dont need it anymore:
+	filePaper.close()
 
-for s in itemlist:
-	if s.attributes['Name'].value == 'LastAuthor':
-		print(s.childNodes[0].nodeValue)
-#    print(s.attributes['Name'].value)
-	#print(s.childNodes[0].nodeValue)
+	# print dataPaper
 
+	domPaper = parseString(dataPaper)
+	itemlist = domPaper.getElementsByTagName('Item') 
+	#print len(itemlist)
+	#print itemlist[0].attributes['Name'].value
 
-#parse the xml you downloaded
-# dom = parseString(dataPaper)
+	for s in itemlist:
+		if s.attributes['Name'].value == 'Title':
+			title = s.childNodes[0].nodeValue
+			#print title
+		if s.attributes['Name'].value == 'PubDate':
+			pubDate = s.childNodes[0].nodeValue
+			#print pubDate
+		if s.attributes['Name'].value == 'LastAuthor':
+			lastAuthor = s.childNodes[0].nodeValue
+			#print lastAuthor
 
-# #retrieve the first xml tag (<tag>data</tag>) that the parser finds with name tagName:
-# authorTag = dom.getElementsByTagName('LastName')[0].toxml()
-# #strip off the tag (<tag>data</tag>  --->   data):
-# authorData=xmlTag.replace('<LastName>','').replace('</LastName>','')
-
-# authorTag = dom.getElementsByTagName('LastAuthor')[0].toxml()
-# #strip off the tag (<tag>data</tag>  --->   data):
-# authorData=xmlTag.replace('<LastAuthor>','').replace('</LastAuthor>','')
-
-# print(authorData)
+	#    print(s.attributes['Name'].value)
+		#print(s.childNodes[0].nodeValue)
 
 
-## -----------------------------------
-## write csv database indexed by paper
-## -----------------------------------
+		# "title":"Chordoma genetic sequencing",
+		# "year": 2004,
+		# "month": 3,
+		# "authors": [12,8,2,5],
+		# "URL": 34545,
+		# "disease": "Chordoma",
+		# "leading": 12,
+		# "last author": 5,
+		# "index": 8
+		
+	#parse the xml you downloaded
+	# dom = parseString(dataPaper)
 
-# import csv
-# csvWriter = csv.writer(open('papersDB.csv', 'ab'), delimiter=',')
+	# #retrieve the first xml tag (<tag>data</tag>) that the parser finds with name tagName:
+	# authorTag = dom.getElementsByTagName('LastName')[0].toxml()
+	# #strip off the tag (<tag>data</tag>  --->   data):
+	# authorData=xmlTag.replace('<LastName>','').replace('</LastName>','')
 
-# for r in range(numExercises):
-    # csvWriter.writerow([name, date, exerciseNames[r], exerciseNums[r]])
+	# authorTag = dom.getElementsByTagName('LastAuthor')[0].toxml()
+	# #strip off the tag (<tag>data</tag>  --->   data):
+	# authorData=xmlTag.replace('<LastAuthor>','').replace('</LastAuthor>','')
+
+	# print(authorData)
+
+
+	## --------------------------------
+	## write json file indexed by paper
+	## --------------------------------
+	
+	#json.dumps(['foo', {'bar': ('baz', None, 1.0, 2)}])
+	print json.dumps({'PMID':PMID, 'title':title, 'title':title, 'disease':disease})
+	# with open('papers.txt', 'aw') as outfile:
+		# json.dump({'PMID':PMID, 'title':title, 'title':title, 'disease':disease}, outfile)
